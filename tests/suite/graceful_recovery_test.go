1fe655989c78f64df8a450c2ea6754ba785304c1
Benjamin Jee <b.jee@f5.com>
2025-03-18 14:47:33 -0700

    Add more retries to graceful recovery test

--- tests/suite/graceful_recovery_test.go
+++ tests/suite/graceful_recovery_test.go
@@ -303,7 +303,7 @@ var _ = Describe("Graceful Recovery test", Ordered, Label("graceful-recovery"), 
 			}).
 			WithTimeout(timeoutConfig.CreateTimeout * 2).
 			WithPolling(500 * time.Millisecond).
-			MustPassRepeatedly(5).
+			MustPassRepeatedly(20).
 			Should(BeTrue())
 
 		nginxPodName := nginxPodNames[0]
