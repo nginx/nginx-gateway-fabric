2829f3b672674132a1d409b6f1849adea2a5a885
Benjamin Jee <b.jee@f5.com>
2025-04-22 14:34:15 -0700

    Change gcr.io to us-docker

--- tests/scripts/push-crossplane-image.sh
+++ tests/scripts/push-crossplane-image.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 source scripts/vars.env
 
-docker tag nginx-crossplane:latest gcr.io/$GKE_PROJECT/nginx-crossplane:latest
-docker push gcr.io/$GKE_PROJECT/nginx-crossplane:latest
+docker tag nginx-crossplane:latest us-docker.pkg.dev/$GKE_PROJECT/nginx-gateway-fabric/nginx-crossplane:latest
+docker push us-docker.pkg.dev/$GKE_PROJECT/nginx-gateway-fabric/nginx-crossplane:latest
