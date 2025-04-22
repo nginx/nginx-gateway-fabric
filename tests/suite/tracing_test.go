a3078ede8103657b59d3f8620e5acff71e8fc06b
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2025-04-22 19:29:44 +0530

    Add support for multiple gateways (#3275)
    Problem: Users want to be able to configure multiple Gateways with a single installation of NGF.
    
    Solution: Support the ability to create multiple Gateways. Routes and policies can be attached to multiple Gateways.
    
    Also fixed conformance tests.
    
    ---------
    
    Co-authored-by: Saylor Berman <s.berman@f5.com>

--- tests/suite/tracing_test.go
+++ tests/suite/tracing_test.go
@@ -92,7 +92,7 @@ var _ = Describe("Tracing", FlakeAttempts(2), Ordered, Label("functional", "trac
 		Expect(resourceManager.ApplyFromFiles(files, namespace)).To(Succeed())
 		Expect(resourceManager.WaitForAppsToBeReady(namespace)).To(Succeed())
 
-		nginxPodNames, err := framework.GetReadyNginxPodNames(k8sClient, namespace, timeoutConfig.GetTimeout)
+		nginxPodNames, err := framework.GetReadyNginxPodNames(k8sClient, namespace, timeoutConfig.GetStatusTimeout)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(nginxPodNames).To(HaveLen(1))
 
