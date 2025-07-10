01718c11dfd98c0bc7f1e4726463b49f83d1ef25
Saylor Berman <s.berman@f5.com>
2025-07-10 12:46:58 -0600

    Don't use default http client

--- tests/framework/request.go
+++ tests/framework/request.go
@@ -56,12 +56,12 @@ func makeRequest(
 ) (*http.Response, error) {
 	dialer := &net.Dialer{}
 
-	baseTransport, ok := http.DefaultTransport.(*http.Transport)
+	transport, ok := http.DefaultTransport.(*http.Transport)
 	if !ok {
 		return nil, errors.New("transport is not of type *http.Transport")
 	}
 
-	customTransport := baseTransport.Clone()
+	customTransport := transport.Clone()
 	customTransport.DialContext = func(
 		ctx context.Context,
 		network,
@@ -94,21 +94,13 @@ func makeRequest(
 
 	var resp *http.Response
 	if strings.HasPrefix(url, "https") {
-		transport, ok := http.DefaultTransport.(*http.Transport)
-		if !ok {
-			return nil, errors.New("transport is not of type *http.Transport")
-		}
-
-		customTransport := transport.Clone()
 		// similar to how in our examples with https requests we run our curl command
 		// we turn off verification of the certificate, we do the same here
 		customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //nolint:gosec // for https test traffic
-		client := &http.Client{Transport: customTransport}
-		resp, err = client.Do(req)
-	} else {
-		resp, err = http.DefaultClient.Do(req)
 	}
 
+	client := &http.Client{Transport: customTransport}
+	resp, err = client.Do(req)
 	if err != nil {
 		return nil, err
 	}
