c7129bd7301f3c84c0a57af47421232112ba265d
shaun-nx <s.odonovan@f5.com>
2025-07-14 11:47:35 +0100

    Add tests for targetRefGroup

--- tests/cel/policies/clientsettingspolicies/targetRef_test.go
+++ tests/cel/policies/clientsettingspolicies/targetRef_test.go
@@ -13,47 +13,13 @@ func TestClientSettingsPoliciesTargetRef(t *testing.T) {
 		"GRPCRoute": true,
 	}
 
-	testInvalidTargetRefs(t, allowedKinds)
 	testValidTargetRefs(t, allowedKinds)
+	testInvalidTargetRefs(t, allowedKinds)
 }
 
-func testInvalidTargetRefs(t *testing.T, allowedKinds map[string]bool) {
-	t.Helper()
-
-	tests := []struct {
-		name       string
-		wantErrors string
-		targetRef  gatewayv1alpha2.LocalPolicyTargetReference
-	}{
-		{
-			name:       "Validate TargetRef is of an allowed kind",
-			wantErrors: "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'",
-			targetRef: gatewayv1alpha2.LocalPolicyTargetReference{
-				Kind:  "InvalidKind",
-				Group: "gateway.networking.k8s.io",
-			},
-		},
-		{
-			name:       "Validate TargetRef is of an allowed kind",
-			wantErrors: "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'",
-			targetRef: gatewayv1alpha2.LocalPolicyTargetReference{
-				Kind:  "TCPRoute",
-				Group: "gateway.networking.k8s.io",
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if _, ok := allowedKinds[string(tt.targetRef.Kind)]; !ok {
-				gotError := "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'"
-
-				if tt.wantErrors != gotError {
-					t.Errorf("Test %s failed: got error %q, want %q", tt.name, gotError, tt.wantErrors)
-				}
-			}
-		})
-	}
+func TestClientSettingsPoliciesTargetRefGroup(t *testing.T) {
+	testValidTargetRefGroup(t)
+	testInvalidTargetRefGroup(t)
 }
 
 func testValidTargetRefs(t *testing.T, allowedKinds map[string]bool) {
@@ -96,6 +62,112 @@ func testValidTargetRefs(t *testing.T, allowedKinds map[string]bool) {
 				gotError := "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'"
 
 				if tt.wantErrors == gotError {
+					t.Errorf("Test %s failed: got error %q, want %q", tt.name, gotError, tt.wantErrors)
+				}
+			}
+		})
+	}
+}
+
+func testInvalidTargetRefs(t *testing.T, allowedKinds map[string]bool) {
+	t.Helper()
+
+	tests := []struct {
+		name       string
+		wantErrors string
+		targetRef  gatewayv1alpha2.LocalPolicyTargetReference
+	}{
+		{
+			name:       "Validate TargetRef is of an allowed kind",
+			wantErrors: "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'",
+			targetRef: gatewayv1alpha2.LocalPolicyTargetReference{
+				Kind:  "InvalidKind",
+				Group: "gateway.networking.k8s.io",
+			},
+		},
+		{
+			name:       "Validate TargetRef is of an allowed kind",
+			wantErrors: "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'",
+			targetRef: gatewayv1alpha2.LocalPolicyTargetReference{
+				Kind:  "TCPRoute",
+				Group: "gateway.networking.k8s.io",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, ok := allowedKinds[string(tt.targetRef.Kind)]; !ok {
+				gotError := "TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute'"
+
+				if tt.wantErrors != gotError {
+					t.Errorf("Test %s failed: got error %q, want %q", tt.name, gotError, tt.wantErrors)
+				}
+			}
+		})
+	}
+}
+
+func testValidTargetRefGroup(t *testing.T) {
+	t.Helper()
+
+	tests := []struct {
+		name           string
+		wantErrors     string
+		targetRefGroup gatewayv1alpha2.LocalPolicyTargetReference
+	}{
+		{
+			name:       "Validate TargetRef group is gateway.networking.k8s.io",
+			wantErrors: "TargetRef Group must be gateway.networking.k8s.io",
+			targetRefGroup: gatewayv1alpha2.LocalPolicyTargetReference{
+				Group: "gateway.networking.k8s.io",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.targetRefGroup.Group != "gateway.networking.k8s.io" {
+				gotError := "TargetRef Group must be gateway.networking.k8s.io"
+
+				if tt.wantErrors == gotError {
+					t.Errorf("Test %s failed: got error %q, want %q", tt.name, gotError, tt.wantErrors)
+				}
+			}
+		})
+	}
+}
+
+func testInvalidTargetRefGroup(t *testing.T) {
+	t.Helper()
+
+	tests := []struct {
+		name           string
+		wantErrors     string
+		targetRefGroup gatewayv1alpha2.LocalPolicyTargetReference
+	}{
+		{
+			name:       "Validate TargetRef group is gateway.networking.k8s.io",
+			wantErrors: "TargetRef Group must be gateway.networking.k8s.io",
+			targetRefGroup: gatewayv1alpha2.LocalPolicyTargetReference{
+				Group: "invalid.networking.k8s.io",
+			},
+		},
+		{
+			name:       "Validate TargetRef is of an allowed kind",
+			wantErrors: "TargetRef Group must be gateway.networking.k8s.io",
+			targetRefGroup: gatewayv1alpha2.LocalPolicyTargetReference{
+				Group: "discovery.k8s.io/v1",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.targetRefGroup.Group != "gateway.networking.k8s.io" {
+				gotError := "TargetRef Group must be gateway.networking.k8s.io"
+
+				if tt.wantErrors != gotError {
 					t.Errorf("Test %s failed: got error %q, want %q", tt.name, gotError, tt.wantErrors)
 				}
 			}
