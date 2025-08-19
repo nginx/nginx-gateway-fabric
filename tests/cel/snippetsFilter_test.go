62a1e991b7d057b4dfd3e5513ef153d29b471631
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2025-08-19 11:39:32 -0600

    remove tests for OpenAPI schema validation fields

--- tests/cel/snippetsFilter_test.go
+++ tests/cel/snippetsFilter_test.go
@@ -8,7 +8,7 @@ import (
 	ngfAPIv1alpha1 "github.com/nginx/nginx-gateway-fabric/v2/apis/v1alpha1"
 )
 
-func TestSnippetsFilterValidation_Snippets(t *testing.T) {
+func TestSnippetsFilterValidation(t *testing.T) {
 	t.Parallel()
 	k8sClient := getKubernetesClient(t)
 
@@ -44,128 +44,8 @@ func TestSnippetsFilterValidation_Snippets(t *testing.T) {
 			},
 		},
 		{
-			name:       "Validate empty snippet value is not allowed",
-			wantErrors: []string{"Invalid value: \"\""},
-			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
-				Snippets: []ngfAPIv1alpha1.Snippet{
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTP,
-						Value:   "",
-					},
-				},
-			},
-		},
-		{
-			name:       "Validate no snippets is not allowed",
-			wantErrors: []string{"Invalid value: 0"},
-			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
-				Snippets: []ngfAPIv1alpha1.Snippet{},
-			},
-		},
-		{
-			name: "Validate maximum number of snippets is allowed",
-			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
-				Snippets: []ngfAPIv1alpha1.Snippet{
-					{
-						Context: ngfAPIv1alpha1.NginxContextMain,
-						Value:   "worker_processes 4;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTP,
-						Value:   "limit_req zone=one burst=5 nodelay;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTPServer,
-						Value:   "server_name example.com;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTPServerLocation,
-						Value:   "deny all;",
-					},
-				},
-			},
-		},
-		{
-			name:       "Validate more than maximum number of snippets is not allowed",
-			wantErrors: []string{"Too many: 5: must have at most 4 items"},
-			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
-				Snippets: []ngfAPIv1alpha1.Snippet{
-					{
-						Context: ngfAPIv1alpha1.NginxContextMain,
-						Value:   "worker_processes 4;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTP,
-						Value:   "limit_req zone=one burst=5 nodelay;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTPServer,
-						Value:   "server_name example.com;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTPServerLocation,
-						Value:   "deny all;",
-					},
-					{
-						Context: ngfAPIv1alpha1.NginxContextHTTPServerLocation,
-						Value:   "extra configuration",
-					},
-				},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			snippetsFilter := &ngfAPIv1alpha1.SnippetsFilter{
-				ObjectMeta: controllerruntime.ObjectMeta{
-					Name:      uniqueResourceName(testResourceName),
-					Namespace: defaultNamespace,
-				},
-				Spec: tt.spec,
-			}
-			validateCrd(t, tt.wantErrors, snippetsFilter, k8sClient)
-		})
-	}
-}
-
-func TestSnippetsFilterValidation_Context(t *testing.T) {
-	t.Parallel()
-	k8sClient := getKubernetesClient(t)
-
-	tests := []struct {
-		name       string
-		wantErrors []string
-		spec       ngfAPIv1alpha1.SnippetsFilterSpec
-	}{
-		{
-			name:       "Validate empty context is not allowed",
-			wantErrors: []string{"Unsupported value: \"\""},
-			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
-				Snippets: []ngfAPIv1alpha1.Snippet{
-					{
-						Context: "",
-						Value:   "empty context configuration",
-					},
-				},
-			},
-		},
-		{
-			name:       "Validate incorrect context is not allowed",
-			wantErrors: []string{"Unsupported value: \"invalid.context\""},
-			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
-				Snippets: []ngfAPIv1alpha1.Snippet{
-					{
-						Context: "invalid.context",
-						Value:   "invalid context configuration",
-					},
-				},
-			},
-		},
-		{
 			name:       "Validate duplicate contexts are not allowed",
-			wantErrors: []string{"Only one snippet allowed per context"},
+			wantErrors: []string{expectedSnippetsFilterContextError},
 			spec: ngfAPIv1alpha1.SnippetsFilterSpec{
 				Snippets: []ngfAPIv1alpha1.Snippet{
 					{
