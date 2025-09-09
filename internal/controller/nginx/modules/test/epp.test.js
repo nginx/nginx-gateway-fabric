9d7ec35cf849de618832266d0dacd95fe1ea0ba2
Saylor Berman <s.berman@f5.com>
2025-09-09 08:04:44 -0600

    Update unit tests and README

--- internal/controller/nginx/modules/test/epp.test.js
+++ internal/controller/nginx/modules/test/epp.test.js
@@ -5,7 +5,7 @@ function makeRequest(body) {
 	let r = {
 		// Test mocks
 		error(msg) {
-			console.log('\tngx_error:', msg);
+			r.variables.error = msg;
 		},
 		requestText: body,
 		variables: {},
@@ -20,22 +20,33 @@ describe('extractModel', () => {
 			name: 'returns the model value',
 			body: '{"model":"gpt-4"}',
 			model: 'gpt-4',
+			error: undefined,
 		},
 		{
 			name: 'returns empty string if model is missing',
 			body: '{"foo":1}',
 			model: '',
+			error: 'request body does not contain model parameter',
 		},
 		{
 			name: 'returns empty string for invalid JSON',
 			body: 'not-json',
 			model: '',
+			error: `error parsing request body for model name: Unexpected token 'o', "not-json" is not valid JSON`,
+		},
+		{
+			name: 'empty request body',
+			body: '',
+			model: '',
+			error: 'error parsing request body for model name: Unexpected end of JSON input',
 		},
 	];
 
 	tests.forEach((test) => {
 		it(test.name, () => {
-			expect(epp.extractModel(makeRequest(test.body))).to.equal(test.model);
+			let r = makeRequest(test.body);
+			expect(epp.extractModel(r)).to.equal(test.model);
+			expect(r.variables.error).to.equal(test.error);
 		});
 	});
 });
