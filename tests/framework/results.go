3eed64074ecbea7d597df0cf7853819c29b0e698
Tina Usova <t.usova@f5.com>
2025-09-10 15:38:47 +0100

    Remove os methods wrappers

--- tests/framework/results.go
+++ tests/framework/results.go
@@ -15,7 +15,7 @@ import (
 
 // CreateResultsDir creates and returns the name of the results directory for a test.
 func CreateResultsDir(testName, version string) (string, error) {
-	pwd, err := GetWorkingDir()
+	pwd, err := os.Getwd()
 	if err != nil {
 		return "", err
 	}
@@ -31,13 +31,12 @@ func CreateResultsDir(testName, version string) (string, error) {
 		}
 	}
 
-	return dirName, MkdirAll(dirName, 0o777)
+	return dirName, os.MkdirAll(dirName, 0o777)
 }
 
 // CreateResultsFile creates and returns the results file for a test.
 func CreateResultsFile(filename string) (*os.File, error) {
-	GinkgoWriter.Printf("Creating results file %q\n", filename)
-	outFile, err := OpenFile(filename, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0o644)
+	outFile, err := os.OpenFile(filename, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0o644)
 	if err != nil {
 		GinkgoWriter.Printf("ERROR occurred during creating results file %q, error: %s\n", filename, err)
 
@@ -89,7 +88,7 @@ func WriteSystemInfoToFile(file *os.File, ci ClusterInfo, plus bool) error {
 }
 
 func generatePNG(resultsDir, inputFilename, outputFilename, configFilename string) error {
-	pwd, err := GetWorkingDir()
+	pwd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
@@ -158,96 +157,6 @@ func WriteContent(resultsFile *os.File, content string) error {
 	return nil
 }
 
-// GetWorkingDir returns the current working directory.
-func GetWorkingDir() (string, error) {
-	pwd, err := os.Getwd()
-	if err != nil {
-		GinkgoWriter.Printf("ERROR occurred during getting current working directory %q, error: %s\n", pwd, err)
-	}
-
-	return pwd, err
-}
-
-// CreateFile creates a new file with the given name.
-func CreateFile(fileName string) (*os.File, error) {
-	file, err := os.Create(fileName)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR occurred during creating file %q, error: %s\n", fileName, err)
-	}
-
-	return file, err
-}
-
-// OpenFile opens an existing file with the given name.
-func OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
-	file, err := os.OpenFile(name, flag, perm)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR occurred during openning results file %q, error: %s\n", name, err)
-	}
-
-	return file, err
-}
-
-// MkdirAll creates a directory with the specified permissions.
-func MkdirAll(path string, perm os.FileMode) error {
-	err := os.MkdirAll(path, perm)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR occurred during creating directory %q, error: %s\n", path, err)
-	}
-
-	return err
-}
-
-// ReadFile reads the contents of a file.
-func ReadFile(file string) ([]byte, error) {
-	result, err := os.ReadFile(file)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR occurred during reading file %q, error: %s\n", file, err)
-	}
-
-	return result, err
-}
-
-// WriteString writes a string to the given file.
-func WriteString(file *os.File, content string) (int, error) {
-	result, err := io.WriteString(file, content)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR writing error log file: %v\n", err)
-	}
-	return result, err
-}
-
-// WriteCSVRecord writes a CSV record using the given writer.
-func WriteCSVRecord(writer *csv.Writer, record []string) error {
-	err := writer.Write(record)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR writing CSV record: %v\n", err)
-	}
-
-	return err
-}
-
-// UserHomeDir returns the user's home directory.
-func UserHomeDir() (string, error) {
-	dir, err := os.UserHomeDir()
-	if err != nil {
-		GinkgoWriter.Printf("ERROR getting user home directory, error: %s\n", err)
-	}
-	GinkgoWriter.Printf("User home directory is %q\n", dir)
-
-	return dir, err
-}
-
-// Remove removes the specified file or empty directory.
-func Remove(name string) error {
-	err := os.Remove(name)
-	if err != nil {
-		GinkgoWriter.Printf("ERROR occurred during removing %q, error: %s\n", name, err)
-	}
-
-	return err
-}
-
 // NewVegetaCSVEncoder returns a vegeta CSV encoder.
 func NewVegetaCSVEncoder(w io.Writer) vegeta.Encoder {
 	return vegeta.NewCSVEncoder(w)
@@ -255,18 +164,18 @@ func NewVegetaCSVEncoder(w io.Writer) vegeta.Encoder {
 
 // NewCSVResultsWriter creates and returns a CSV results file and writer.
 func NewCSVResultsWriter(resultsDir, fileName string, resultHeaders ...string) (*os.File, *csv.Writer, error) {
-	if err := MkdirAll(resultsDir, 0o750); err != nil {
+	if err := os.MkdirAll(resultsDir, 0o750); err != nil {
 		return nil, nil, err
 	}
 
-	file, err := OpenFile(filepath.Join(resultsDir, fileName), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o644)
+	file, err := os.OpenFile(filepath.Join(resultsDir, fileName), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o644)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	writer := csv.NewWriter(file)
 
-	if err = WriteCSVRecord(writer, resultHeaders); err != nil {
+	if err = writer.Write(resultHeaders); err != nil {
 		return nil, nil, err
 	}
 
