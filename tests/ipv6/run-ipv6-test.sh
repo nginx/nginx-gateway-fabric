643a7b0352fd52946a5d9ce8109beb603d0a95d6
shaun-nx <s.odonovan@f5.com>
2025-09-19 11:23:39 +0100

    Update script to use make commands. Update make commands with new variables for user to override

--- tests/ipv6/run-ipv6-test.sh
+++ tests/ipv6/run-ipv6-test.sh
@@ -2,18 +2,20 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
-RELEASE=$1
-RELEASE_IMAGE=$2
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+REPO_DIR=$(dirname $(dirname "$SCRIPT_DIR"))
 
-if [[ -z $RELEASE || -z $RELEASE_IMAGE ]]; then
-    echo "Usage: $0 <RELEASE> <RELEASE_IMAGE> [HELM_RELEASE_NAME] [NAMESPACE] [CLUSTER_NAME]"
-    echo "Error: RELEASE and RELEASE_IMAGE are required parameters. Example usage $(make ipv6-test RELEASE=vX.Y.Z RELEASE_IMAGE=release-X.Y-rc)"
+TAG=$1
+
+if [[ -z $TAG ]]; then
+    echo "Usage: $0 <TAG> [RELEASE_NAME] [NAMESPACE] [CLUSTER_NAME]"
+    echo "Error: TAG is a required parameter. Example usage: `make ipv6-test TAG=release-X.Y-rc`"
     exit 1
 fi
 
-HELM_RELEASE_NAME=${3:-ngf}
-NAMESPACE=${4:-nginx-gateway}
-CLUSTER_NAME=${5:-ipv6-only-${RELEASE}}
+RELEASE_NAME=${2:-nginx-gateway}
+NAMESPACE=${3:-nginx-gateway}
+CLUSTER_NAME=${4:-ipv6-only-${TAG}}
 RELEASE_REPO=ghcr.io/nginx/nginx-gateway-fabric
 
 cleanup() {
@@ -25,27 +27,23 @@ trap cleanup EXIT
 
 kind create cluster --name ${CLUSTER_NAME} --config ipv6/config/kind-ipv6-only.yaml
 
-echo "== Applying Gateway API CRDs"
-kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=${RELEASE}" | kubectl apply -f -
-
 echo "== Installing NGINX Gateway Fabric..."
-echo "== Using NGF from ${RELEASE_REPO}:${RELEASE_IMAGE}..."
-echo "== Using NGINX from ${RELEASE_REPO}/nginx:${RELEASE_IMAGE}..."
+echo "== Using NGF from ${RELEASE_REPO}:${TAG}..."
+echo "== Using NGINX from ${RELEASE_REPO}/nginx:${TAG}..."
 
-helm install ${HELM_RELEASE_NAME} --wait oci://ghcr.io/nginx/charts/nginx-gateway-fabric \
-    --create-namespace -n ${NAMESPACE} \
-    --set nginx.config.ipFamily=ipv6 \
-    --set nginx.service.type=ClusterIP \
-    --set nginxGateway.image.repository=${RELEASE_REPO} \
-    --set nginxGateway.image.tag=${RELEASE_IMAGE} \
-    --set nginx.image.repository=${RELEASE_REPO}/nginx \
-    --set nginx.image.tag=${RELEASE_IMAGE}
+HELM_PARAMETERS="--set nginx.config.ipFamily=ipv6"
+make helm-install-local HELM_PARAMETERS="${HELM_PARAMETERS}" \
+  PREFIX="${RELEASE_REPO}" \
+  TAG="${TAG}" \
+  SELF_DIR="${REPO_DIR}/" \
+  NGINX_SERVICE_TYPE="ClusterIP" \
+  PULL_POLICY="Always"
 
 echo "== Deploying Gateway..."
 kubectl apply -f ipv6/manifests/gateway.yaml
 
 kubectl wait --for=condition=accepted --timeout=300s gateway/gateway
-POD_NAME=$(kubectl get pods -l app.kubernetes.io/instance=${HELM_RELEASE_NAME} -o jsonpath='{.items[0].metadata.name}')
+POD_NAME=$(kubectl get pods -l app.kubernetes.io/instance=${RELEASE_NAME} -o jsonpath='{.items[0].metadata.name}')
 kubectl wait --for=condition=ready --timeout=300s pod/${POD_NAME}
 
 echo "== Deploying IPv6 test application"
@@ -54,7 +52,7 @@ kubectl apply -f ipv6/manifests/ipv6-test-app.yaml
 echo "== Waiting for test applications to be ready..."
 kubectl wait --for=condition=available --timeout=300s deployment/test-app-ipv6
 
-echo " ==Getting NGF service IPv6 address from gateway status"
+echo "== Getting NGF service IPv6 address from gateway status"
 NGF_IPV6=$(kubectl get gateway -o jsonpath='{.items[0].status.addresses[0].value}')
 echo "NGF IPv6 Address: $NGF_IPV6"
 
