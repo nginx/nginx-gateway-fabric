00b418df6fe2838daa072930f83bfdc855e7e641
shaun-nx <s.odonovan@f5.com>
2025-10-09 19:24:10 +0100

    Fix //nolint lint error

--- internal/controller/telemetry/collector.go
+++ internal/controller/telemetry/collector.go
@@ -39,10 +39,8 @@ type ConfigurationGetter interface {
 
 // Data is telemetry data.
 //
-// nolint
-//
 //go:generate go run -tags generator github.com/nginx/telemetry-exporter/cmd/generator -type=Data -scheme -scheme-protocol=NGFProductTelemetry -scheme-df-datatype=ngf-product-telemetry
-type Data struct {
+type Data struct { //nolint
 	// ImageSource tells whether the image was built by GitHub or locally (values are 'gha', 'local', or 'unknown')
 	ImageSource string
 	tel.Data    // embedding is required by the generator.
