557af0846f57d1614152567132f9d8d50a85ce52
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2025-10-16 17:25:20 -0600

    add experimental for inference extension

--- tests/scripts/check-pod-exit-code.sh
+++ tests/scripts/check-pod-exit-code.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-# Check if a pod name is provided as an argument
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <pod-name>"
     exit 1
@@ -10,7 +9,6 @@ fi
 
 POD_NAME=$1
 
-# Get the exit code of the specified pod
 CODE=$(kubectl get pod "${POD_NAME}" -o jsonpath='{.status.containerStatuses[].state.terminated.exitCode}')
 if [ "${CODE}" -ne 0 ]; then
     exit 2
