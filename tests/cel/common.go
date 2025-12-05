6bee60ceaac5b868ca73681dd118912836896c4e
Tina U. <tataruty@outlook.com>
2025-12-05 11:01:46 +0000

    Add CEL tests for AuthenticationFilter (#4396)
    Overview
    As a user of NGF, I would like to add CEL tests for the AuthenticationFilter CR so that I can be confident that the CEL validation works as I expect
    
    Acceptance
    Add new test file to tests/cel directory for the AuthenticationFilter
    Validations to test
    for type=Basic, spec.basic must be set

--- tests/cel/common.go
+++ tests/cel/common.go
@@ -33,44 +33,46 @@ const (
 	emptyGroup     = ""
 )
 
-// ClientSettingsPolicy validation errors.
 const (
-	expectedTargetRefKindError       = `TargetRef Kind must be one of: Gateway, HTTPRoute, or GRPCRoute`
-	expectedTargetRefGroupError      = `TargetRef Group must be gateway.networking.k8s.io`
-	expectedHeaderWithoutServerError = `header can only be specified if server is specified`
-)
+	// AuthenticationFilter validation errors.
+	expectedBasicRequiredError = `for type=Basic, spec.basic must be set`
 
-// NginxProxy validation errors.
-const (
-	expectedOneOfDeploymentOrDaemonSetError = `only one of deployment or daemonSet can be set`
-	expectedIfModeSetTrustedAddressesError  = `if mode is set, trustedAddresses is a required field`
-	expectedMinReplicasLessThanOrEqualError = `minReplicas must be less than or equal to maxReplicas`
-)
+	// Kind validation errors.
+	expectedTargetRefKindMustBeGatewayOrHTTPRouteOrGrpcRouteError = "TargetRef Kind must be one of: " +
+		"Gateway, HTTPRoute, or GRPCRoute"
+	expectedTargetRefKindMustBeHTTPRouteOrGrpcRouteError = "TargetRef Kind must be: HTTPRoute or GRPCRoute"
+	expectedTargetRefKindServiceError                    = "TargetRefs Kind must be: Service"
 
-// ObservabilityPolicy validation errors.
-const (
-	expectedTargetRefMustBeHTTPRouteOrGrpcRouteError = `TargetRef Kind must be: HTTPRoute or GRPCRoute`
-	expectedTargetRefKindAndNameComboMustBeUnique    = `TargetRef Kind and Name combination must be unique`
-	expectedStrategyMustBeOfTypeRatio                = `ratio can only be specified if strategy is of type ratio`
-)
+	// Group validation errors.
+	expectedTargetRefGroupError     = "TargetRef Group must be gateway.networking.k8s.io"
+	expectedTargetRefGroupCoreError = "TargetRefs Group must be core"
 
-// UpstreamSettingsPolicy validation errors.
-const (
-	expectedTargetRefKindServiceError = `TargetRefs Kind must be: Service`
-	expectedTargetRefGroupCoreError   = `TargetRefs Group must be core`
-	expectedTargetRefNameUniqueError  = `TargetRef Name must be unique`
-)
+	// Name uniqueness validation errors.
+	expectedTargetRefNameUniqueError              = "TargetRef Name must be unique"
+	expectedTargetRefKindAndNameComboMustBeUnique = "TargetRef Kind and Name combination must be unique"
 
-// SnippetsFilter validation errors.
-const (
-	expectedSnippetsFilterContextError = `Only one snippet allowed per context`
-)
+	// Header validation error.
+	expectedHeaderWithoutServerError = "header can only be specified if server is specified"
 
-const (
+	// Deployment/DaemonSet validation error.
+	expectedOneOfDeploymentOrDaemonSetError = "only one of deployment or daemonSet can be set"
+
+	// TrustedAddresses/Mode validation error.
+	expectedIfModeSetTrustedAddressesError = "if mode is set, trustedAddresses is a required field"
+
+	// Replicas validation error.
+	expectedMinReplicasLessThanOrEqualError = "minReplicas must be less than or equal to maxReplicas"
+
+	// Strategy validation error.
+	expectedStrategyMustBeOfTypeRatio = "ratio can only be specified if strategy is of type ratio"
+
+	// SnippetsFilter validation errors.
+	expectedSnippetsFilterContextError = "Only one snippet allowed per context"
+
+	// Namespace for tests.
 	defaultNamespace = "default"
-)
 
-const (
+	// Test resource names.
 	testResourceName  = "test-resource"
 	testTargetRefName = "test-targetRef"
 )
