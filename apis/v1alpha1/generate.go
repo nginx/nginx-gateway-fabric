a8878dd709d325751b01364418df43ae36561f0e
majiayu000 <1835304752@qq.com>
2026-01-01 16:52:13 +0800

    feat: add code generation for NGF Policy methods
    Add code generation tooling to automatically generate the
    GetTargetRefs, GetPolicyStatus, and SetPolicyStatus methods
    for NGF Policy types.
    
    This replaces the manually written policy_methods.go files
    with generated zz_generated.policymethods.go files.
    
    Changes:
    - Add policygen generator under apis/generate/policygen/
    - Add go:generate directives in v1alpha1 and v1alpha2 packages
    - Remove manual policy_methods.go files
    - Generate new zz_generated.policymethods.go files
    
    Fixes #1939
    
    Signed-off-by: majiayu000 <1835304752@qq.com>

---
+++ apis/v1alpha1/generate.go
@@ -0,0 +1,3 @@
+package v1alpha1
+
+//go:generate go run ../generate/policygen/main.go -types=ClientSettingsPolicy,UpstreamSettingsPolicy -package=v1alpha1 -targetref-single=ClientSettingsPolicy -output=zz_generated.policymethods.go
---
+++ apis/v1alpha2/generate.go
@@ -0,0 +1,3 @@
+package v1alpha2
+
+//go:generate go run ../generate/policygen/main.go -types=ObservabilityPolicy -package=v1alpha2 -output=zz_generated.policymethods.go
