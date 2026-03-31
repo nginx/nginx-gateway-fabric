f9d6ca1089f7ad6444082703292bb609b0846395
salonichf5 <146118978+salonichf5@users.noreply.github.com>
2026-03-31 16:08:25 -0600

    update the comment and proposal doc

--- apis/v1alpha1/clientsettingspolicy_types.go
+++ apis/v1alpha1/clientsettingspolicy_types.go
@@ -106,7 +106,7 @@ type ClientKeepAlive struct {
 	//nolint:lll
 	Timeout *ClientKeepAliveTimeout `json:"timeout,omitempty"`
 
-	// MinimumTimeout defines the timeout for which the keep-alive client connection
+	// MinTimeout defines the timeout for which the keep-alive client connection
 	// will not be closed on the server side for connection reuse or on
 	// graceful shutdown of worker processes.
 	// Default: https://nginx.org/en/docs/http/ngx_http_core_module.html#keepalive_min_timeout.
