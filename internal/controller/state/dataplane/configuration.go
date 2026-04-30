a97765ab086c21f545a46c996509988e42922150
Shaun <s.odonovan@f5.com>
2026-04-30 11:27:52 +0100

    Remove listener name from ssl_client_certificate file name (#5206)

--- internal/controller/state/dataplane/configuration.go
+++ internal/controller/state/dataplane/configuration.go
@@ -473,14 +473,13 @@ func buildFrontendTLSCertBundles(
 			continue
 		}
 		// Create a unique cert bundle ID for this listener gateway combo.
-		// e.g. cert_bundle_default_gateway_443_https
-		// for a listener on port 443 named "https" on a gateway in the default namespace.
+		// e.g. cert_bundle_default_gateway_443 for a HTTPS listener on port 443
+		// for a gateway in the default namespace.
 		caCertRef := types.NamespacedName{
 			Namespace: gateway.Source.Namespace,
-			Name: fmt.Sprintf("%s_%d_%s",
+			Name: fmt.Sprintf("%s_%d",
 				gateway.Source.Name,
 				listener.Source.Port,
-				listener.Name,
 			),
 		}
 		id := generateCertBundleID(caCertRef)
