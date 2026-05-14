1ef5d137c496b57be47c63aaef3d0b1150d718cd
oyiz-michael <oyiz.ugn@gmail.com>
2026-05-15 00:02:40 +0100

    fix: sync zz_generated.deepcopy.go import style with controller-gen output

--- apis/v1alpha2/zz_generated.deepcopy.go
+++ apis/v1alpha2/zz_generated.deepcopy.go
@@ -6,9 +6,9 @@ package v1alpha2
 
 import (
 	"github.com/nginx/nginx-gateway-fabric/v2/apis/v1alpha1"
-	v2 "k8s.io/api/autoscaling/v2"
+	"k8s.io/api/autoscaling/v2"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	apisv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
