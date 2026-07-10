76e6818de94530e983f57c5b328f2737889b14dd
Saloni Choudhary <146118978+salonichf5@users.noreply.github.com>
2026-07-10 10:32:42 -0700

    add ExternalLoadBalancers CRD to repo (#5562)
    Problem: Users want a way to be able to specify gateway link configuration to configure Big-IP using Nginx Gateway Fabric
    
    Solution: Adds a ew CRD named externalloadbalancers to specify gatewayLink configuration to specify Big-ip configuration.

--- apis/v1alpha1/register.go
+++ apis/v1alpha1/register.go
@@ -50,6 +50,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&RateLimitPolicyList{},
 		&WAFPolicy{},
 		&WAFPolicyList{},
+		&ExternalLoadBalancers{},
+		&ExternalLoadBalancersList{},
 	)
 	// AddToGroupVersion allows the serialization of client types like ListOptions.
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
