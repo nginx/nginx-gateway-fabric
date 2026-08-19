3986835141f97340bdb9c2d80f2d6471adc4581f
Nutsa Bidzishvili <n.bidzishvili@f5.com>
2026-08-19 12:13:35 +0100

    fix default path setting

--- internal/controller/state/dataplane/configuration.go
+++ internal/controller/state/dataplane/configuration.go
@@ -2661,13 +2661,12 @@ func buildAccessLog(srcLogSettings *ngfAPIv1alpha2.NginxLogging) *AccessLog {
 			srcLogSettings.AccessLog.Destination != nil {
 			accessLog := &AccessLog{
 				Format: *srcLogSettings.AccessLog.Format,
-				Path:   DefaultAccessLogPath,
 			}
 			if srcLogSettings.AccessLog.Escape != nil {
 				accessLog.Escape = string(*srcLogSettings.AccessLog.Escape)
-			}
-			if srcLogSettings.AccessLog.Destination != nil {
-				setAccessLogDestination(accessLog, srcLogSettings.AccessLog.Destination)
+				if srcLogSettings.AccessLog.Destination != nil {
+					setAccessLogDestination(accessLog, srcLogSettings.AccessLog.Destination)
+				}
 			}
 			return accessLog
 		}
