3992f7788cab8521d1d2dcb4129be5c197de5a0a
Nutsa Bidzishvili <n.bidzishvili@f5.com>
2026-08-20 11:13:28 +0100

    fix merge conflicts

--- apis/v1alpha2/nginxproxy_types.go
+++ apis/v1alpha2/nginxproxy_types.go
@@ -443,7 +443,7 @@ type NginxLogging struct {
 	AgentLevel *AgentLogLevel `json:"agentLevel,omitempty"`
 
 	// AccessLog defines the access log settings, including format itself and disabling option.
-	// For now only path /dev/stdout can be used.
+	// Default path /dev/stdout can be used or file/syslog destinations can be configured.
 	//
 	// +optional
 	AccessLog *NginxAccessLog `json:"accessLog,omitempty"`
@@ -524,7 +524,7 @@ type NginxAccessLog struct {
 
 	// Format specifies the custom log format string.
 	// If not specified, NGINX default 'combined' format is used.
-	// For now only path /dev/stdout can be used.
+	// Default path /dev/stdout can be used or file/syslog destinations can be configured.
 	// Single quotes and line breaks are not allowed because the format is
 	// rendered inside a single-quoted NGINX log_format directive.
 	// See https://nginx.org/en/docs/http/ngx_http_log_module.html#log_format
@@ -541,6 +541,12 @@ type NginxAccessLog struct {
 	//
 	// +optional
 	Escape *NginxAccessLogEscapeType `json:"escape,omitempty"`
+
+	// Destination specifies where the access logs are sent to.
+	// If file/syslog not specified, access logs are by default sent to /dev/stdout.
+	//
+	// +optional
+	Destination *NginxAccessLogDestination `json:"destination,omitempty"`
 }
 
 // NginxAccessLogEscapeType defines the escape setting for variables in access log format.
@@ -561,6 +567,50 @@ const (
 	// NginxAccessLogEscapeNone disables escaping of characters.
 	NginxAccessLogEscapeNone NginxAccessLogEscapeType = "none"
 )
+
+// NginxAccessLogDestination defines the destination for access logs.
+type NginxAccessLogDestination struct {
+	// File specifies the file destination for access logs.
+	// Only valid when type is set to "file".
+	//
+	// +optional
+	File *NginxAccessLogFile `json:"file,omitempty"`
+
+	// Syslog specifies the syslog destination for access logs.
+	// Only valid when type is set to "syslog".
+	//
+	// +optional
+	Syslog *NginxAccessLogSyslog `json:"syslog,omitempty"`
+
+	// Type specifies the type of destination for access logs.
+	//
+	// +kubebuilder:default=file
+	Type *NginxAccessLogDestinationType `json:"type"`
+}
+
+// NginxAccessLogDestinationType defines the supported destination types for access logs.
+//
+// +kubebuilder:validation:Enum=file;syslog
+type NginxAccessLogDestinationType string
+
+const (
+	// NginxAccessLogDestinationTypeFile writes access logs to a specified file path.
+	NginxAccessLogDestinationTypeFile NginxAccessLogDestinationType = "file"
+	// NginxAccessLogDestinationTypeSyslog writes access logs to a syslog server.
+	NginxAccessLogDestinationTypeSyslog NginxAccessLogDestinationType = "syslog"
+)
+
+// NginxAccessLogFile specifies the file destination for access logs.
+type NginxAccessLogFile struct {
+	Path *string `json:"path,omitempty"`
+}
+
+// NginxAccessLogSyslog specifies the syslog destination for access logs.
+type NginxAccessLogSyslog struct {
+	// Server is the syslog server address in the format "hostname:port" or "IP:port".
+	// +kubebuilder:validation:Pattern=`^[a-zA-Z0-9.-]+:[0-9]+$`
+	Server string `json:"server"`
+}
 
 // NginxPlus specifies NGINX Plus additional settings. These will only be applied if NGINX Plus is being used.
 type NginxPlus struct {
